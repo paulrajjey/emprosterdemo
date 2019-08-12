3484c01b6db191b3b2b27ed2568e23cf3468e92e
rhdmAdmin <>
2019-08-12 12:07:13 -0500

    {/src/main/java/employeerostering/employeerostering/DayOffRequest.java}

--- src/main/java/employeerostering/employeerostering/DayOffRequest.java
+++ src/main/java/employeerostering/employeerostering/DayOffRequest.java
@@ -127,6 +127,17 @@ public class DayOffRequest implements java.io.Serializable {
 		this.timeOffHrs = timeOffHrs;
 		this.empId = empId;
 	}
+	public DayOffRequest(
+			java.time.LocalDate date, java.lang.String type,
+			java.time.LocalTime startTime, java.time.LocalTime endTime,
+			 int empId) {
+		this.employee = employee;
+		this.date = date;
+		this.type = type;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.empId = empId;
+	}
 	public DayOffRequest(java.time.LocalDate date, java.lang.String type, int empId) {
 		this.date = date;
 		this.type = type;
