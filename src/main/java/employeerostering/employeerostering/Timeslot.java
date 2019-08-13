8a0dd6aee243943b42374713a3acc55f7453482f
rhdmAdmin <>
2019-08-13 11:54:46 -0500

    {/src/main/java/employeerostering/employeerostering/Timeslot.java}

--- src/main/java/employeerostering/employeerostering/Timeslot.java
+++ src/main/java/employeerostering/employeerostering/Timeslot.java
@@ -16,6 +16,13 @@
 
 package employeerostering.employeerostering;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 
 @XStreamAlias("Timeslot")
 /**
